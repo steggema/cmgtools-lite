33f6055712cf83445e531dcdd2e06edca3fa0c54
Gael <gael.touquet@gmail.com>
2018-06-11 13:12:39 +0200

    fix to use cmgtools out of CERN

--- RootTools/python/samples/ComponentCreator.py
+++ RootTools/python/samples/ComponentCreator.py
@@ -4,7 +4,7 @@ from CMGTools.Production.dataset import createDataset, createMyDataset
 import re
 
 class ComponentCreator(object):
-    def makeMCComponent(self,name,dataset,user,pattern,xSec=1,useAAA=False,unsafe=False):
+    def makeMCComponent(self,name,dataset,user,pattern,xSec=1,useAAA=True,unsafe=False):
         
          component = cfg.MCComponent(
              dataset=dataset,
