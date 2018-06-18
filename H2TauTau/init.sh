db1e7e1b883533985f7596de31f1f29572a67533
Colin <colin.bernet@cern.ch>
2018-06-18 10:05:41 +0200

    added initialization script

---
+++ H2TauTau/init.sh
@@ -0,0 +1,10 @@
+echo 'set cms environment'
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+echo 'set crab3 environment'
+source /cvmfs/cms.cern.ch/crab3/crab.sh
+cd ../..
+cmsenv
+echo 'set cmssw environment: $CMSSW_RELEASE_BASE'
+cd CMGTools/H2TauTau
+export X509_USER_PROXY=$HOME/private/cms.proxy
+mkdir -p $HOME/private
